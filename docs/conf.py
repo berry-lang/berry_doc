a494f28c15aa724dc822440b04f03bfe9fb0c6b0
Stephan Hadinger <stephan.hadinger@gmail.com>
2024-05-14 21:00:00 +0200

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -76,6 +76,7 @@ import sphinx_typo3_theme
 #
 html_theme = 'sphinx_typo3_theme'
 html_logo = 'berry-logo.png'
+html_favicon = 'favicon.ico'
     
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
