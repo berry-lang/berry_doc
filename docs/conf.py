655b0ab0b501009ce80f48caf17e1981860b2ad6
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2025-05-11 21:07:20 +0200

    Use sphinx_wagtail_theme

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = [
         'rst2pdf.pdfbuilder',
         'breathe',
         'sphinx_typo3_theme',
-        'sphinx-wagtail-theme',
+        'sphinx_wagtail_theme',
         'myst_parser'
 ]
 
@@ -74,12 +74,12 @@ subprocess.call('doxygen', shell=True)
 # -- Options for HTML output -------------------------------------------------
 
 #import sphinx_typo3_theme
-import sphinx-wagtail-theme
+import sphinx_wagtail_theme
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx-wagtail-theme'
+html_theme = 'sphinx_wagtail_theme'
 html_favicon = 'favicon.ico'
     
 # Theme options are theme-specific and customize the look and feel of a theme
